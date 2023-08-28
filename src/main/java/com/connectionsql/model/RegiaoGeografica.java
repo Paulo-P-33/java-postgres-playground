d4e64ac4095ba8b5c954ac7da394f0dd893cbc8b
Luiz Paulo <luiz.lira029@gmail.com>
2023-08-28 12:36:26 +0000

    feat: created RegiaoGeografica class

---
+++ src/main/java/com/connectionsql/model/RegiaoGeografica.java
@@ -0,0 +1,18 @@
+package com.connectionsql.model;
+
+public class RegiaoGeografica {
+    private Long id;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    private String name;
+}
